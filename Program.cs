3fea70404c4255a92e064d60bac10c48ab3f1f5e
BED-COM-14-20 <bed-com-14-20@unima.ac.mw>
2023-12-11 12:07:47 +0200

    implemented switch statement

--- Program.cs
+++ Program.cs
@@ -1,36 +1,80 @@
-﻿ class Program
+﻿ using System;
+
+class Program
 {
     static void Main()
     {
         // Create a BinaryTree of Person objects
         BinaryTree<Person> personTree = new BinaryTree<Person>();
 
-        // Insert some persons into the tree
-        personTree.Insert(new Person("John", 30));
-        personTree.Insert(new Person("Jane", 25));
-        personTree.Insert(new Person("Bob", 35));
-
-        // Print the inorder traversal of the tree
-        Console.WriteLine("Inorder Traversal:");
-        personTree.PrintInorder();
-
-        // Search for a person in the tree
-        Person searchPerson = new Person("Jane", 25);
-        if (personTree.Search(searchPerson, out var foundNode, out var parentNode))
+        while (true)
         {
-            Console.WriteLine($"Person found: {foundNode.Data.Name}, Age: {foundNode.Data.Age}");
-        }
-        else
-        {
-            Console.WriteLine("Person not found");
-        }
+            Console.WriteLine("\nOptions:");
+            Console.WriteLine("1. Add a person");
+            Console.WriteLine("2. Search for a person");
+            Console.WriteLine("3. Remove a person");
+            Console.WriteLine("4. Print Inorder Traversal");
+            Console.WriteLine("5. Exit");
 
-        // Remove a person from the tree
-        Person removePerson = new Person("John", 30);
-        personTree.Remove(removePerson);
+            Console.Write("Enter your choice (1-5): ");
+            string choice = Console.ReadLine();
 
-        // Print the inorder traversal again after removal
-        Console.WriteLine("\nInorder Traversal after removal:");
-        personTree.PrintInorder();
+            switch (choice)
+            {
+                case "1":
+                    Console.Write("Enter name: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Enter age: ");
+                    int age = int.Parse(Console.ReadLine());
+
+                    personTree.Insert(new Person(name, age));
+                    break;
+
+                case "2":
+                    Console.Write("Enter name to search: ");
+                    string searchName = Console.ReadLine();
+
+                    Console.Write("Enter age to search: ");
+                    int searchAge = int.Parse(Console.ReadLine());
+
+                    Person searchPerson = new Person(searchName, searchAge);
+
+                    if (personTree.Search(searchPerson, out var foundNode, out var parentNode))
+                    {
+                        Console.WriteLine($"Person found: {foundNode.Data.Name}, Age: {foundNode.Data.Age}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Person not found");
+                    }
+                    break;
+
+                case "3":
+                    Console.Write("Enter name to remove: ");
+                    string removeName = Console.ReadLine();
+
+                    Console.Write("Enter age to remove: ");
+                    int removeAge = int.Parse(Console.ReadLine());
+
+                    Person removePerson = new Person(removeName, removeAge);
+                    personTree.Remove(removePerson);
+                    break;
+
+                case "4":
+                    Console.WriteLine("Inorder Traversal:");
+                    personTree.PrintInorder();
+                    break;
+
+                case "5":
+                    Console.WriteLine("Exiting the program.");
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                    break;
+            }
+        }
     }
 }
